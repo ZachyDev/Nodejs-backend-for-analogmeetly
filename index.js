b5116e84ce4a89626b36da49faa0eaf29b2fee72
ZachyDev <mosetizachary001@gmail.com>
2020-08-31 14:03:00 +0300

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let port = process.env.PORT || 1234;
 let app = express();
-app.get('/',(req,res,next) => {
+app.get('/meetly',(req,res,next) => {
     res.json({
         "meeting_details": [{
             "date": "10/02/2020",
