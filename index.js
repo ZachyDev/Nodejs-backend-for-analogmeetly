3e8fc3a58a1420dd0273cf036cd0bc60e0ff1813
ZachyDev <mosetizachary001@gmail.com>
2020-08-31 15:43:20 +0300

    added a resource to the endpoint

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-let express = require('express');
-let port = process.env.PORT || 1234;
-let app = express();
-app.get('/meetly',(req,res,next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Credentials", "true");
-		res.setHeader("Access-Control-Max-Age", "1800");
-		res.setHeader("Access-Control-Allow-Headers", "content-type");
-		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-    res.json({
-        "meeting_details": [{
-            "date": "10/02/2020",
-            "type": "AGM",
-            "department": "finance",
-            "attendees": "20",
-            "users": "analog",
-            "company": "analogteams"
-        }]
-    })
-});
-app.listen(port,() => {
-    console.log('its live on heroku!');
+document.addEventListener('submit',(event) => {
+    event.preventDefault();
+    // call getInputs
+    let type = getInputs('type');
+    let date = getInputs('date');
+    let dept = getInputs('dept');
+    let attendees = getInputs('attendees');
+    // *************************node**********************
+        let express = require('express');
+        let port = process.env.PORT || 1234;
+        let app = express();
+        app.get('/meetly',(req,res,next) => {
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Max-Age", "1800");
+                res.setHeader("Access-Control-Allow-Headers", "content-type");
+                res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+                res.send('./index.html');
+            res.json({
+                "meeting_details": [{
+                    "date": date,
+                    "type":type,
+                    "department": dept,
+                    "attendees": attendees,
+                }]
+            })
+        });
+        app.listen(port,() => {
+            console.log('its live on heroku!');
+        })
+    
+    //  ************************end***********************
+    
 })
+
+// getInputs
+const getInputs = (id) => {
+    return document.getElementById(id).value;
+}
