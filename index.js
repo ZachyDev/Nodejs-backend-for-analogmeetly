0da8866e02da4356c9b00dc03487aabb6a159441
ZachyDev <mosetizachary001@gmail.com>
2020-08-31 15:49:18 +0300

    added a resource to the endpoint

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-document.addEventListener('submit',(event) => {
-    event.preventDefault();
-    // call getInputs
-    let type = getInputs('type');
-    let date = getInputs('date');
-    let dept = getInputs('dept');
-    let attendees = getInputs('attendees');
-    // *************************node**********************
-        let express = require('express');
-        let port = process.env.PORT || 1234;
-        let app = express();
-        app.get('/meetly',(req,res,next) => {
-                res.setHeader("Access-Control-Allow-Origin", "*");
-                res.setHeader("Access-Control-Allow-Credentials", "true");
-                res.setHeader("Access-Control-Max-Age", "1800");
-                res.setHeader("Access-Control-Allow-Headers", "content-type");
-                res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-            res.json({
-                "meeting_details": [{
-                    "date": date,
-                    "type":type,
-                    "department": dept,
-                    "attendees": attendees,
-                }]
-            })
-        });
-        app.listen(port,() => {
-            console.log('its live on heroku!');
-        })
-    
-    //  ************************end***********************
-    
-})
+// document.addEventListener('submit',(event) => {
+//     event.preventDefault();
+//     // call getInputs
+//     let type = getInputs('type');
+//     let date = getInputs('date');
+//     let dept = getInputs('dept');
+//     let attendees = getInputs('attendees');
+// });
 
-// getInputs
-const getInputs = (id) => {
-    return document.getElementById(id).value;
-}
+// // getInputs
+// const getInputs = (id) => {
+//     return document.getElementById(id).value;
+// }
+
+ // *************************node**********************
+ let express = require('express');
+ let port = process.env.PORT || 1234;
+ let app = express();
+ app.get('/meetly',(req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    res.json({
+         "meeting_details": [{
+             "date": date,
+             "type":type,
+             "department": dept,
+             "attendees": attendees,
+         }]
+     })
+ });
+ app.listen(port,() => {
+     console.log('its live on heroku!');
+ })
+
+//  ************************end***********************
