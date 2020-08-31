c0bc1c7b05eeb9b699b6260a0fe70859b7376114
ZachyDev <mosetizachary001@gmail.com>
2020-08-31 14:42:52 +0300

    added a resource to the endpoint

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let express = require('express');
 let port = process.env.PORT || 1234;
 let app = express();
 app.get('/meetly',(req,res,next) => {
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.json({
         "meeting_details": [{
             "date": "10/02/2020",
@@ -10,7 +14,7 @@ app.get('/meetly',(req,res,next) => {
             "department": "finance",
             "attendees": "20",
             "users": "analog",
-            "company": "analog"
+            "company": "analogteams"
         }]
     })
 });
