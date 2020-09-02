2ea2ab82fbf26362d06d646479189bf286de2189
ZachyDev <mosetizachary001@gmail.com>
2020-09-02 21:31:12 +0300

    set up basic routes

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
  let express = require('express');
  let port = process.env.PORT || 1234;
  let app = express();
- app.get('/meetly',(req,res,next) => {
+ let meetings = [
+     {
+        "date": "01/01/2020",
+        "type":"Weekly",
+        "department": "Devops",
+        "attendees": 6
+    },
+    {
+       "date": "02/01/2020",
+       "type":"AGM",
+       "department": "management",
+       "attendees": 8
+   },
+   {
+       "date": "03/01/2020",
+       "type":"Weekly",
+       "department": "frontend",
+       "attendees": 22
+   },{
+       "date": "04/01/2020",
+       "type":"Weekly",
+       "department": "backend",
+       "attendees": 11
+   },{
+       "date": "05/01/2020",
+       "type":"monthly",
+       "department": "design",
+       "attendees": 10
+   },{
+       "date": "06/01/2020",
+       "type":"Weekly",
+       "department": "finance",
+       "attendees":5
+   }];
+
+ app.get('/meetly', (req, res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-    res.json({
-         "meeting_details": [{
-             "date": "01/01/2020",
-             "type":"Weekly",
-             "department": "Devops",
-             "attendees": 6
-         },
-         {
-            "date": "02/01/2020",
-            "type":"AGM",
-            "department": "management",
-            "attendees": 8
-        },
-        {
-            "date": "03/01/2020",
-            "type":"Weekly",
-            "department": "frontend",
-            "attendees": 22
-        },{
-            "date": "04/01/2020",
-            "type":"Weekly",
-            "department": "backend",
-            "attendees": 11
-        },{
-            "date": "05/01/2020",
-            "type":"monthly",
-            "department": "design",
-            "attendees": 10
-        },{
-            "date": "06/01/2020",
-            "type":"Weekly",
-            "department": "finance",
-            "attendees":5
-        }]
-     })
+    response.json(meetings);
+  });
+ app.get('/meetly/:weekly',(req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    const date = Number(request.params.date);
+    const getDate = accounts.find((meeting) => meeting.date === date);
+    res.json(getDate);
  });
  app.listen(port,() => {
      console.log('its live on heroku!');
