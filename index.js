f11fadbd68d0ce61e127b27464a098de31c81207
ZachyDev <mosetizachary001@gmail.com>
2020-09-02 20:22:03 +0300

    updated the api

--- index.js
+++ index.js
@@ -6,13 +6,33 @@
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.json({
-         "meeting_details": [{
-             "date": "31/08/2020",
-             "type":"Weekly",
-             "department": "Devops",
-             "attendees": 22,
-         }]
-     })
+        "users": [
+            {
+              "date": "01/01/2020",
+              "type": "AGM",
+              "department": "DevOps",
+              "attendees": "20",
+            },
+            {
+              "date": "02/01/2020",
+              "type": "weekly",
+              "department": "Frontend",
+              "attendees": "10",
+             },
+             {
+              "date": "03/01/2020",
+              "type": "monthly",
+              "department": "Backend",
+              "attendees": "11",
+            },
+            {
+             "date": "04/01/2020",
+             "type": "monthly",
+             "department": "finance",
+             "attendees": "5",
+           }
+        ]
+    })
  });
  app.listen(port,() => {
      console.log('its live on heroku!');
