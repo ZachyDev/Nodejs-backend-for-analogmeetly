9550ec081ff664a00c3c6d05fbcb396b2ddf2aab
ZachyDev <mosetizachary001@gmail.com>
2020-09-03 00:33:36 +0300

    edited routes

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
 let express = require('express');
 let port = 3000
 let app = express();
-let meetings = [
-        {
-            "date": "01/01/2020",
-            "type": "AGM",
-            "department": "devops",
-            "attendees": "20",
-        },
-        {
-            "date": "02/01/2020",
-            "type": "weekly",
-            "department": "frontend",
-            "attendees": "10",
-        },
-        {
-            "date": "03/01/2020",
-            "type": "monthly",
-            "department": "backend",
-            "attendees": "11",
-        },
-        {
-            "date": "04/01/2020",
-             "type": "monthly",
-             "department": "finance",
-             "attendees": "5",
-        }
-];
+let meetings = require('./store');
 // all meetings
 app.get(`/meetings`, (request, response) => {
   response.json(meetings);
 });
+// meeting type
+app.get(`/meetings/:type`, (request, response) => {
+    const type = String(request.params.type);
+    const getType = meetings.find((meeting) => meeting.type === type);
+    if (!getType) {
+      response.status(500).send('Meeting not found.')
+    } else {
+      response.json(getType);
+    }
+  });
 // meeting by department
-app.get(`/meetings/:department`, (request, response,next) => {
+app.get(`/meetings/:department`, (request, response) => {
     const dept = String(request.params.department);
     const getDept = meetings.find((meeting) => meeting.department === dept);
   
@@ -41,19 +26,7 @@ app.get(`/meetings/:department`, (request, response,next) => {
     } else {
       response.json(getDept);
     }
-    next()
   })
-// meeting type
-app.get(`/meetings/:type`, (request, response,next) => {
-  const type = String(request.params.type);
-  const getType = meetings.find((meeting) => meeting.type === type);
-
-  if (!getType) {
-    response.status(500).send('Meeting not found.')
-  } else {
-    response.json(getType);
-  }
-});
 app.listen(port,() => {
     console.log('live on heroku')
 })
---
+++ store.js
@@ -0,0 +1,27 @@
+let meetings = [
+    {
+        "date": "01/01/2020",
+        "type": "AGM",
+        "department": "devops",
+        "attendees": "20",
+    },
+    {
+        "date": "02/01/2020",
+        "type": "weekly",
+        "department": "frontend",
+        "attendees": "10",
+    },
+    {
+        "date": "03/01/2020",
+        "type": "monthly",
+        "department": "backend",
+        "attendees": "11",
+    },
+    {
+        "date": "04/01/2020",
+         "type": "monthly",
+         "department": "finance",
+         "attendees": "5",
+    }
+];
+module.exports = meetings;
