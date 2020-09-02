b44a22f1de60f8686874d19713dee1c2aaf6905d
ZachyDev <mosetizachary001@gmail.com>
2020-09-02 20:37:05 +0300

    updated resources

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
  let express = require('express');
- let port = process.env.PORT || 1234;
+//  let port = process.env.PORT || 1234;
+let port = 3000;
  let app = express();
  app.get('/meetly',(req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.json({
-         "meeting_details": [{
-             "date": "31/08/2020",
-             "type":"Weekly",
-             "department": "Devops",
-             "attendees": 22
+         "meetings": [{
+            "date": "01/01/2020",
+              "type": "AGM",
+              "department": "DevOps",
+              "attendees": "20",
          },
          {
-            "date": "31/08/2020",
-            "type":"Weekly",
-            "department": "Devops",
-            "attendees": 22
-        }]
+            "date": "02/01/2020",
+            "type": "weekly",
+            "department": "Frontend",
+            "attendees": "10",
+        },
+        {
+            "date": "03/01/2020",
+            "type": "monthly",
+            "department": "Backend",
+            "attendees": "11",
+          },
+          {
+           "date": "04/01/2020",
+           "type": "monthly",
+           "department": "finance",
+           "attendees": "5",
+         }
+    ]
      })
  });
  app.listen(port,() => {
